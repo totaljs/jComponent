4a549c63c79d599cc658600a3196b6e2292fa4b0
Peter Širka <petersirka@gmail.com>
2015-12-08 08:01:12 +0100

    Updated minification.

--- minify.sh
+++ minify.sh
@@ -1,6 +1,6 @@
 ECHO "[COMPILING]"
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
-# uglifyjs jcomponent.js -c -o jcomponent.min.js
 # uglifyjs jcomponent.js -c -m -o jcomponent.min.js
+uglifyjs jcomponent.js -c -o jcomponent.min.js
 total --minify jcomponent.js
