a9bff43a5ee5a7adac8e46caf439166653d457d4
Peter Širka <petersirka@gmail.com>
2017-02-07 09:16:46 +0100

    Cleaning code.

--- minify.sh
+++ minify.sh
@@ -1,4 +1,4 @@
 ECHO "[COMPILING]"
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
-uglifyjs jcomponent.js --quotes=1 -c -m -o jcomponent.min.js
+uglifyjs jcomponent.js --quotes=1 -c -m -o jc.min.js
