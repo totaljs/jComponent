cc31e5451838b30e2f879d6603d7947eebe38ba6
Peter Širka <petersirka@gmail.com>
2018-05-23 13:40:22 +0200

    Improved binder.

--- jc.js
+++ jc.js
@@ -3559,16 +3559,16 @@
 
 		for (var i = 0; i < arr.length - 1; i++) {
 			var item = arr[i];
-			binder.push('binders[\'' + item + '\']&&binderbind(\'' + item + '\',\'' + path + '\')');
+			binder.push('binders[\'' + item + '\']&&binderbind(\'' + item + '\',\'' + path + '\',$ticks)');
 		}
 
 		var v = arr[arr.length - 1];
-		binder.push('binders[\'' + v + '\']&&binderbind(\'' + v + '\',\'' + path + '\')');
+		binder.push('binders[\'' + v + '\']&&binderbind(\'' + v + '\',\'' + path + '\',$ticks)');
 
 		if (v.substring(0, 1) !== '[')
 			v = '.' + v;
 
-		var fn = (new Function('w', 'a', 'b', 'binders', 'binderbind', builder.join(';') + ';var v=typeof(a)===\'function\'?a(MAIN.compiler.get(b)):a;w' + v + '=v;' + binder.join(';') + ';return v'));
+		var fn = (new Function('w', 'a', 'b', 'binders', 'binderbind', 'var $ticks=Math.random().toString().substring(2,8);' + builder.join(';') + ';var v=typeof(a)===\'function\'?a(MAIN.compiler.get(b)):a;w' + v + '=v;' + binder.join(';') + ';return v'));
 		paths[key] = fn;
 		fn(W, value, path, binders, binderbind);
 		return C;
@@ -8334,11 +8334,14 @@
 		return value;
 	});
 
-	function binderbind(path, absolutePath) {
+	function binderbind(path, absolutePath, ticks) {
 		var arr = binders[path];
 		for (var i = 0; i < arr.length; i++) {
 			var item = arr[i];
-			item.exec(GET(item.path), absolutePath);
+			if (item.ticks !== ticks) {
+				item.ticks = ticks;
+				item.exec(GET(item.path), absolutePath);
+			}
 		}
 	}
 
