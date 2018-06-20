5faff517b0dab727a51c1f6f447730b5edf62707
Peter Širka <petersirka@gmail.com>
2018-06-20 11:07:37 +0200

    Updated features.

--- jc.js
+++ jc.js
@@ -53,6 +53,7 @@
 	var blocked = {};
 	var storage = {};
 	var extensions = {}; // COMPONENT_EXTEND()
+	var configs = [];
 	var cache = {};
 	var fallback = { $: 0 }; // $ === count of new items in fallback
 	var fallbackpending = [];
@@ -142,7 +143,7 @@
 	MR.format = /\{\d+\}/g;
 
 	M.loaded = false;
-	M.version = 'v14.053';
+	M.version = 'v14.054';
 	M.$localstorage = 'jc';
 	M.$version = '';
 	M.$language = '';
@@ -2728,6 +2729,11 @@
 				obj.type = attrcom(el, 'type') || '';
 				obj.id = attrcom(el, 'id') || obj._id;
 				obj.siblings = all.length > 1;
+
+				for (var i = 0; i < configs.length; i++) {
+					var con = configs[i];
+					con.fn(obj) && obj.reconfigure(con.config, NOOP);
+				}
 
 				com.declaration.call(obj, obj, obj.config);
 				meta[3] && el.attrd('jc-value', meta[3]);
@@ -5722,6 +5728,37 @@
 		}
 
 		W.RECOMPILE();
+	};
+
+	W.COMPONENT_CONFIG = function(selector, config) {
+
+		if (typeof(selector) === 'string') {
+			var fn = [];
+			selector.split(' ').forEach(function(sel) {
+				var prop = '';
+				switch (sel.trim().substring(0, 1)) {
+					case '*':
+						fn.push('com.path.indexOf(\'{0}\')!==-1'.format(sel.substring(1)));
+						return;
+					case '.':
+						// path
+						prop = 'path';
+						break;
+					case '#':
+						// id
+						prop = 'id';
+						break;
+					default:
+						// name
+						prop = '$name';
+						break;
+				}
+				fn.push('com.{0}==\'{1}\''.format(prop, prop === '$name' ? sel : sel.substring(1)));
+			});
+			selector = FN('com=>' + fn.join('&&'));
+		}
+
+		configs.push({ fn: selector, config: config });
 	};
 
 	W.COMPONENT = function(name, config, declaration, dependencies) {
