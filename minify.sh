f498656d901f28e6538c3c2f71a5486af24fa4f7
petersirka <petersirka@gmail.com>
2024-07-07 09:36:15 +0200

    jComponent v20.

--- minify.sh
+++ minify.sh
@@ -1,4 +1,6 @@
-ECHO "[COMPILING]"
+echo "Minifying ..."
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
-uglifyjs jc.js --config-file minify.json -o jc.min.js
+uglifyjs library.js --config-file minify.json -o library.min.js
+cat helpers.js library.min.js > spa.min@20.js
+rm library.min.js
