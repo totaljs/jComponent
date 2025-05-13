8d604e2847e475bb89e6e68151e813cd87243e7b
petersirka <petersirka@gmail.com>
2025-05-13 13:46:01 +0200

    Updated Total.js Icons.

---
+++ makeiconlist.js
@@ -0,0 +1,22 @@
+require('total4');
+const Fs = require('fs');
+var filename = 'selection.json';
+
+Fs.readFile(filename, function(err, buffer) {
+
+	if (err) {
+		console.error(err);
+		return;
+	}
+
+	var items = JSON.parse(buffer.toString('utf8'));
+	var list = [];
+
+	for (var item of items.icons) {
+		var name = item.properties.name.split(',').trim();
+		list.push(name[0]);
+	}
+
+	list.sort();
+	Fs.writeFile('icons-db.html', list.join(','), function() {});
+});
